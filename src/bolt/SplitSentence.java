cdd75d587d92a4ce2e30e78bdf7bf3ed312cf119
Guangyu Wu <Wu@dhcp-892b1a3a.ucd.ie>
2013-04-10 15:43:44 +0100

    go

---
+++ src/bolt/SplitSentence.java
@@ -0,0 +1,23 @@
+package bolt;
+
+import java.util.Map;
+
+import backtype.storm.task.ShellBolt;
+import backtype.storm.topology.IRichBolt;
+import backtype.storm.topology.OutputFieldsDeclarer;
+
+public class SplitSentence extends ShellBolt implements IRichBolt {
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Map<String, Object> getComponentConfiguration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
