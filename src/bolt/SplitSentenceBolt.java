480440b1e69c0dfceda188c67201f4b1a5a4c3cf
Guangyu Wu <Wu@dhcp-892b1a3a.ucd.ie>
2013-04-10 16:24:24 +0100

    working version

---
+++ src/bolt/SplitSentenceBolt.java
@@ -0,0 +1,27 @@
+package bolt;
+
+import backtype.storm.topology.BasicOutputCollector;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseBasicBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class SplitSentenceBolt extends BaseBasicBolt {
+
+	@Override
+	public void execute(Tuple input, BasicOutputCollector collector) {
+		String sentence = input.getString(0).toLowerCase();
+		for (String word : sentence.split(" ")) {
+			word = word.trim();
+			if (!word.isEmpty()) {
+				collector.emit(new Values(word));
+			}
+		}
+	}
+
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declare(new Fields("word"));
+	}
+}
